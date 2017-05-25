9b2128fd7c673c7afb084537554fd93d27b6e9a3
Stefan Johnson <sj@scrimpycat.io>
2017-05-26 08:58:17 +1000

    Add migration for entities and entity relationships

---
+++ apps/gingerbread_service/priv/repo/migrations/20170525224131_entity.exs
@@ -0,0 +1,35 @@
+defmodule Gingerbread.Service.Repo.Migrations.Entity do
+    use Ecto.Migration
+
+    def change do
+        create table(:entities) do
+            add :identity, :uuid,
+                null: false
+
+            add :entity, :uuid,
+                null: false
+
+            add :active, :boolean,
+                default: true,
+                null: false
+
+            add :name, :string
+
+            timestamps()
+        end
+
+        create index(:entities, [:identity], unique: false)
+        create index(:entities, [:entity], unique: true)
+        create index(:entities, [:name], unique: false)
+
+        create table(:entity_dependants) do
+            add :parent_id, references(:entities),
+                null: false
+
+            add :entity_id, references(:entities),
+                null: false
+
+            timestamps()
+        end
+    end
+end
