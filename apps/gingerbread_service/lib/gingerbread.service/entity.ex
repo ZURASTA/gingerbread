a45af4a4934a84e398db64f75edc6963bad25873
Stefan Johnson <sj@scrimpycat.io>
2017-05-27 23:46:13 +1000

    Document entities

--- apps/gingerbread_service/lib/gingerbread.service/entity.ex
+++ apps/gingerbread_service/lib/gingerbread.service/entity.ex
@@ -1,4 +1,15 @@
 defmodule Gingerbread.Service.Entity do
+    @moduledoc """
+      Manages unique (optionally tagged) entities.
+
+      Entities are unique IDs that are associated with an identity and can have a tag to
+      help identify them. Entities can be connected to other entities in order to map out
+      various relationships between the entities.
+
+      An entities unique ID will be static and remain unique for the entire lifetime of
+      the database. This means that destroyed entities, only destroys the entity itself
+      but does not make that ID re-available.
+    """
     use GenServer
 
     alias Gingerbread.Service.Entity
@@ -24,6 +35,13 @@ defmodule Gingerbread.Service.Entity do
     defp unique_entity({ :error, %{ errors: [entity: _] } }), do: unique_entity(Gingerbread.Service.Repo.insert(Entity.Model.insert_changeset(%Entity.Model{})))
     defp unique_entity(entity), do: entity
 
+    @doc """
+      Create a unique entity with a name (to help classify it) and an identity for it
+      to be associated with.
+
+      Returns `{ :ok, { name, entity } }` if the entity was created. Otherwise returns
+      the reason for failure.
+    """
     @spec create(uuid, atom | nil) :: { :ok, { atom | nil, uuid } } | { :error, String.t }
     def create(identity, name \\ nil) do
         case unique_entity(Gingerbread.Service.Repo.insert(Entity.Model.insert_changeset(%Entity.Model{}, %{ identity: identity, name: if(name != nil, do: to_string(name), else: nil) }))) do
@@ -35,6 +53,14 @@ defmodule Gingerbread.Service.Entity do
         end
     end
 
+    @doc """
+      Destroy an entity.
+
+      After destruction an entity can no longer be used. But it's unique ID will remain,
+      so services that depend on that ID being unique can work unaffected.
+
+      Returns `:ok` if the entity was destroyed. Otherwise returns the reason for failure.
+    """
     @spec destroy(uuid) :: :ok | { :error, String.t }
     def destroy(entity_id) do
         entity_query = from entity in Entity.Model,
@@ -58,6 +84,12 @@ defmodule Gingerbread.Service.Entity do
         end
     end
 
+    @doc """
+      Transfer an entity to a new identity.
+
+      Returns `:ok` if the entity was transferred. Otherwise returns the reason for
+      failure.
+    """
     @spec transfer(uuid, uuid) :: :ok | { :error, String.t }
     def transfer(entity_id, identity) do
         query = from entity in Entity.Model,
@@ -72,6 +104,14 @@ defmodule Gingerbread.Service.Entity do
         end
     end
 
+    @doc """
+      Create a parent-child relationship between two entities.
+
+      Entity relationships can consist of any form (tree, circular, self, etc.).
+
+      Returns `:ok` if the relationship was created. Otherwise returns the reason for
+      failure.
+    """
     @spec add_child(uuid, uuid) :: :ok | { :error, String.t }
     def add_child(parent, child) do
         parent_query = from entity in Entity.Model,
@@ -91,6 +131,12 @@ defmodule Gingerbread.Service.Entity do
         end
     end
 
+    @doc """
+      Remove a parent-child relationship between two entities.
+
+      Returns `:ok` if the relationship was removed. Otherwise returns the reason for
+      failure.
+    """
     @spec remove_child(uuid, uuid) :: :ok | { :error, String.t }
     def remove_child(parent, child) do
         query = from relationship in Entity.Relationship.Model,
@@ -106,6 +152,12 @@ defmodule Gingerbread.Service.Entity do
         end
     end
 
+    @doc """
+      Get the entities associated with the given identity.
+
+      Returns the list of entities belonging to the identity, where each entity is a
+      tagged entity `{ name, entity }`.
+    """
     @spec entities(uuid) :: [{ atom | nil, uuid }]
     def entities(identity) do
         query = from entity in Entity.Model,
@@ -119,6 +171,12 @@ defmodule Gingerbread.Service.Entity do
         end)
     end
 
+    @doc """
+      Get the children of an entity.
+
+      Returns the list of entities belonging to the entity, where each entity is a tagged
+      entity `{ name, entity }`.
+    """
     @spec dependants(uuid) :: [{ atom | nil, uuid }]
     def dependants(entity) do
         query = from relationship in Entity.Relationship.Model,
@@ -133,6 +191,11 @@ defmodule Gingerbread.Service.Entity do
         end)
     end
 
+    @doc """
+      Get the name of an entity.
+
+      Returns `{ :ok, name }` if successful. Otherwise returns the reason for failure.
+    """
     @spec name(uuid) :: { :ok, atom | nil } | { :error, String.t }
     def name(entity_id) do
         query = from entity in Entity.Model,
